05046aada4b1fa56fb52e31761906160d9d66e7c
Barry DeZonia <bdezonia@gmail.com>
2024-11-16 20:26:41 -0600

    Add a file header to a file that needs it

--- src/main/java/nom/bdezonia/zorbage/nmr/Main.java
+++ src/main/java/nom/bdezonia/zorbage/nmr/Main.java
@@ -1,7 +1,33 @@
+/*
+ * zorbage-nmr: : code for populating NMR file data into zorbage structures for further processing
+ *
+ * Copyright (C) 2023 Barry DeZonia
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package nom.bdezonia.zorbage.nmr;
 
 import nom.bdezonia.zorbage.misc.DataBundle;
 
+/**
+ * @author Barry DeZonia
+ */
 public class Main {
 	
 	public static void main(String[] args) {
