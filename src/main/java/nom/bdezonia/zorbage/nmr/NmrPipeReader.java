4c969ca22c466d251176a65aa14c18341cfac0a3
Barry DeZonia <bdezonia@gmail.com>
2025-12-07 20:00:25 -0600

    Update copyrights

--- src/main/java/nom/bdezonia/zorbage/nmr/NmrPipeReader.java
+++ src/main/java/nom/bdezonia/zorbage/nmr/NmrPipeReader.java
@@ -1,7 +1,7 @@
 /*
  * zorbage-nmr: : code for populating NMR file data into zorbage structures for further processing
  *
- * Copyright (C) 2023 Barry DeZonia
+ * Copyright (C) 2023-2026 Barry DeZonia
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
